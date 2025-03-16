ad8a217a4799e0f09b366952264c6ca5561e8a84
ARkhan06 <arkhan060320045@gmail.com>
2025-03-16 02:50:54 -0700

    port updated

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
